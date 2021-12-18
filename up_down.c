aaa60c76655056bf5b131881dbeae63bceaeef3f
Patrick FLORENTIN <pflorent@e1r9p1.42.fr>
2021-12-18 06:24:50 -0800

    checkarg

---
+++ up_down.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   up_down.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pflorent <pflorent@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/18 06:24:17 by pflorent          #+#    #+#             */
+/*   Updated: 2021/12/18 06:24:29 by pflorent         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./includes/push_swap.h"
+
+static int	get_top(int min, int max, t_env *piles)
+{
+	int	pos;
+
+	pos = 1;
+	while (piles->a->prev)
+		piles->a = piles->a->prev;
+	while (piles->a->next)
+	{
+		if (piles->a->pos >= min && piles->a->pos <= max)
+			return (pos);
+		piles->a = piles->a->next;
+		pos++;
+	}
+	if (piles->a->pos < min || piles->a->pos > max)
+		return (0);
+	return (pos);
+}
+
+static int	get_bot(int min, int max, t_env *piles)
+{
+	int	pos;
+
+	pos = 1;
+	while (piles->a->next)
+		piles->a = piles->a->next;
+	while (piles->a->prev)
+	{
+		if (piles->a->pos >= min && piles->a->pos <= max)
+			return (pos);
+		piles->a = piles->a->prev;
+		pos++;
+	}
+	if (piles->a->pos < min || piles->a->pos > max)
+		return (0);
+	return (pos);
+}
+
+int	up_or_down(int min, int max, t_env *piles)
+{
+	int	top;
+	int	bot;
+
+	top = get_top(min, max, piles);
+	bot = get_bot(min, max, piles);
+	printf("\n   from top = %d	     from bot = %d \n\n", top, bot);
+	while (piles->a->prev)
+		piles->a = piles->a->prev;
+	if (!top || !bot)
+		return (0);
+	else if (top <= bot)
+		return (top);
+	else
+		return (-bot);
+}
