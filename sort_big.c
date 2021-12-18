98fefdd22f9e1de5dda49dbb459e73bc9189622e
Patrick FLORENTIN <pflorent@e1r9p1.42.fr>
2021-12-18 08:10:09 -0800

    checkarg

--- sort_big.c
+++ sort_big.c
@@ -6,7 +6,7 @@
 /*   By: pflorent <pflorent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/18 07:34:02 by pflorent          #+#    #+#             */
-/*   Updated: 2021/12/18 07:59:16 by pflorent         ###   ########.fr       */
+/*   Updated: 2021/12/18 08:07:36 by pflorent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,23 @@
 
 t_cmd	*sort_big (t_env *piles, int argc)
 {
-	long 	pos;
-	long	stack;
+	int 	pos;
+	int		stack;
 	t_cmd	ops;
 
 	stack = 1;
+	pos = 1;
 	ops = /* TO INIT */;
 	while (argc > 0)
 	{
-		pos = up_or_down(stack, stack + 20, piles);
+		while (pos && argc > 0)
+		{
+			pos = up_or_down(stack, stack + 20, piles);
 
 
 
-
-
+		}
+		stack += 20;
 	}
 	while (ops->prev)
 		ops = ops->prev;
